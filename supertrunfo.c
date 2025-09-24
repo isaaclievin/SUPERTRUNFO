6039536d6515e79259a52ade0f39c8d370781b1f
isaaclievin <isaac_lievin@hotmail.com>
2025-09-24 19:18:01 +0000

    ACRESCENTEI NOVAS FUNCIONALIDADES DO NIVEL AVENTUREIRO

--- supertrunfo.c
+++ supertrunfo.c
@@ -16,6 +16,10 @@ int main() {
     float pib2;
     int ptur1;
     int ptur2;
+    float dpop1;
+    float dpop2;
+    float pibpc1;
+    float pibpc2;
     //SOLICITEI A ENTRADA DE DADOS POR PARTE DO USUÁRIO COM USO DO TECLADO E SALVEI OS DADOS (CARTA 1)
     printf("Digite uma letra de A a H para representar o Estado da Carta 1: \n");
     scanf("%2s", estado1);
@@ -31,6 +35,9 @@ int main() {
     scanf("%f", &pib1);
     printf("Digite a quantidade de Pontos Turísticos existentes na Cidade da Carta 1: \n");
     scanf("%d", &ptur1);
+    //CALCULO DE DENSIDADE POPULACIONAL (POPULAÇÃO DIVIDIDO POR ÁREA) E PIB PER CAPTA (PIB DIVIDIDO POR POPULAÇÃO) REFERENTE A CARTA 1
+    dpop1 = populacao1 / area1;
+    pibpc1 = pib1 / populacao1;
     //SOLICITEI A ENTRADA DE DADOS POR PARTE DO USUÁRIO COM USO DO TECLADO E SALVEI OS DADOS (CARTA 2)
     printf("Digite uma letra de A a H para representar o Estado da Carta 2: \n");
     scanf(" %2s", estado2);
@@ -46,24 +53,31 @@ int main() {
     scanf("%f", &pib2);
     printf("Digite a quantidade de Pontos Turísticos existentes na Cidade da Carta 2: \n");
     scanf("%d", &ptur2);
+    //CALCULO DE DENSIDADE POPULACIONAL (POPULAÇÃO DIVIDIDO POR ÁREA) E PIB PER CAPTA (PIB DIVIDIDO POR POPULAÇÃO) REFERENTE A CARTA 2
+    dpop2 = populacao2 / area2;
+    pibpc2 = pib2 / populacao2;
     //APRESENTEI AS INFORMAÇÕES SEPARADAS E DESCRITAS (CARTA 1)
     printf("CARTA 1: \n");
     printf("Estado: %s \n", estado1);
     printf("Código da Carta: %s \n", codcarta1);
     printf("Cidade: %s \n", cidade1);
-    printf("População: %d \n", populacao1);
-    printf("Área: %f \n", area1);
-    printf("PIB: %f \n", pib1);
+    printf("População: %d Habitantes \n", populacao1);
+    printf("Área: %.2f Km2 \n", area1);
+    printf("PIB: %.2f Bilhões de Reais \n", pib1);
     printf("Quantidade de Pontos Turísticos: %d \n", ptur1);
+    printf("A Densidade Populacional da Cidade 1 é: %.2f Hab/Km2 \n", dpop1);
+    printf("O PIB Per Capta da Cidade 1 é: %.2f Reais \n", pibpc1);
     //APRESENTEI AS INFORMAÇÕES SEPARADAS E DESCRITAS (CARTA 2)
     printf("CARTA 2: \n");
     printf("Estado: %s \n", estado2);
     printf("Código da Carta: %s \n", codcarta2);
     printf("Cidade: %s \n", cidade2);
-    printf("População: %d \n", populacao2);
-    printf("Área: %f \n", area2);
-    printf("PIB: %f \n", pib2);
-    printf("Quantidade de Pontos Turísticos: %d \n", ptur2);    
+    printf("População: %d Habitantes \n", populacao2);
+    printf("Área: %.2f Km2 \n", area2);
+    printf("PIB: %.2f Bilhões de Reais \n", pib2);
+    printf("Quantidade de Pontos Turísticos: %d \n", ptur2);
+    printf("A Densidade Populacional da Cidade 2 é: %.2f Hab/Km2 \n", dpop2);
+    printf("O PIB Per Capta da Cidade 2 é: %.2f Reais \n", pibpc2);    
     return 0;
 }
 
